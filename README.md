ea73a05d4d5c8d13fc80bed7536b3a5308bff70f
Aryansabasana <aryan.sabasana.cd@gmail.com>
2025-08-20 08:12:44 +0530

    Updated project documentation and added nnew features

---
+++ README.md
@@ -0,0 +1,5 @@
+
+## Project Files
+- README.md: Project Documentation
+- style.css: Project styling
+- script.js: Project functionality
--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 function sayHello() {
      console.log('Hello from codingGita! ');
 }
+
+function show ProjectInfo() {
+  alert('Welcome to codningGita! ');
+}
